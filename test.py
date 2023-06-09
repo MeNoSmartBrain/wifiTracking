2646a48932c56da06d85bf7645a34dae08ebd6ca
yhemmer <yannic.hemmer@stud.tu-darmstadt.de>
2023-06-10 00:48:43 +0200

    Save

--- test.py
+++ test.py
@@ -8,13 +8,36 @@ m = folium.Map(location=[11.0, 11.0], zoom_start=13)
 icon_create_function = '''
     function(cluster) {
         return L.divIcon({
-            html: '<div style="background-color: red; color: white;">' +
-                  cluster.getChildCount() + '</div>',
+            html: '<div class="circle-marker-cluster'''+str(cIndex)+'''">' +
+                '<span>' + cluster.getChildCount() + '</span></div>',
             className: 'marker-cluster-custom',
             iconSize: new L.Point(40, 40)
         });
     }
 '''
+
+# Define the CSS styles for the custom icon
+css_style = '''
+    .circle-marker-cluster'''+str(cIndex)+''' {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        background-color: '''+ hexColors[cIndex] +''';
+        border-radius: 50%;
+        color: white;
+        font-weight: bold;
+        font-size: 14px;
+    }
+    .circle-marker-cluster'''+str(cIndex)+''' span {
+        display: inline-block;
+        margin-top: 2px;
+    }
+'''
+
+# Add the custom CSS styles to the map
+m.get_root().header.add_child(folium.Element('<style>{}</style>'.format(css_style)))
 
 # Create a marker cluster with the custom icon function
 marker_cluster = MarkerCluster(icon_create_function=icon_create_function)
