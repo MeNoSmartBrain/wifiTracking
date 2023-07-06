71166d64447b9237f370fb6c4fc6c9cd696cefbe
yhemmer <yannic.hemmer@stud.tu-darmstadt.de>
2023-07-07 01:07:36 +0200

    State of presentation minor spelling fixes

--- probequest.sh
+++ probequest.sh
@@ -5,9 +5,9 @@
 # sudo ip link set wlp0s20f0u4u1 up
 
 # Fake probe request
-probequest --fake wlp0s20f3 -o $(pwd)/wifiData.csv
+# probequest --fake wlp0s20f0u2 -o $(pwd)/wifiData.csv
 
 # Real probe request
-# probequest wlp0s20f3 -o $(pwd)/result.csv
+probequest wlp0s20f0u2 -o $(pwd)/wifiData.csv
 
 chown toxsos $(pwd)/wifiData.csv 
