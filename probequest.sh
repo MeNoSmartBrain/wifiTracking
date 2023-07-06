ef707c12c3752066d98966b0800939cc7e5e7f2b
yhemmer <yannic.hemmer@stud.tu-darmstadt.de>
2023-07-06 09:39:33 +0200

    Beautfy project and create example data.

--- probequest.sh
+++ probequest.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# ifconfig
+# sudo ip link set wlp0s20f0u4u1 down
+# sudo iw wlp0s20f0u4u1 set monitor none
+# sudo ip link set wlp0s20f0u4u1 up
 
 # Fake probe request
 probequest --fake wlp0s20f3 -o $(pwd)/wifiData.csv
 
 # Real probe request
 # probequest wlp0s20f3 -o $(pwd)/result.csv
+
+chown toxsos $(pwd)/wifiData.csv 
